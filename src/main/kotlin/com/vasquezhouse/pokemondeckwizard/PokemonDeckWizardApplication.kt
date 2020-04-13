acc1b0d45e2915573a34c803bb326d4300fa9101
Santiago Vasquez <svasquez@pivotal.io>
2020-04-13 15:45:02 -0400

    create project via spring initializer

---
+++ src/main/kotlin/com/vasquezhouse/pokemondeckwizard/PokemonDeckWizardApplication.kt
@@ -0,0 +1,11 @@
+package com.vasquezhouse.pokemondeckwizard
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PokemonDeckWizardApplication
+
+fun main(args: Array<String>) {
+	runApplication<PokemonDeckWizardApplication>(*args)
+}
---
+++ src/test/kotlin/com/vasquezhouse/pokemondeckwizard/PokemonDeckWizardApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.vasquezhouse.pokemondeckwizard
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PokemonDeckWizardApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
