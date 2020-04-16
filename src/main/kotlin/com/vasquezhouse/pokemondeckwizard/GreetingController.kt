2ac83b61a309092c31db21f14efeeaecfe5358f8
Santiago Vasquez <svasquez@pivotal.io>
2020-04-16 14:04:32 -0400

    change greeting to spanish

--- src/main/kotlin/com/vasquezhouse/pokemondeckwizard/GreetingController.kt
+++ src/main/kotlin/com/vasquezhouse/pokemondeckwizard/GreetingController.kt
@@ -12,7 +12,7 @@ class GreetingController {
 
     @GetMapping("/greeting")
     fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String) =
-            Greeting(counter.incrementAndGet(), "Hello, $name")
+            Greeting(counter.incrementAndGet(), "Hola, $name")
 }
 
 data class Greeting(val id: Long, val content: String)
